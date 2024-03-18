e58247e85f78b7caa133b58c1b2669d2579f6827
jaiminpatel1999 <jdp9662@gmail.com>
2024-03-18 11:27:34 -0400

    Initial Commit

---
+++ Lab 7 GPSUITests/Lab_7_GPSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab_7_GPSUITestsLaunchTests.swift
+//  Lab 7 GPSUITests
+//
+//  Created by user237118 on 3/18/24.
+//
+
+import XCTest
+
+final class Lab_7_GPSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
